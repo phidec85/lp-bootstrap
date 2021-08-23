9c83160ab9d8cdd2e83ed122280676497ae76a8c
Denis Lemm <denis@Denis-Mac-mini.local>
2021-08-23 16:03:32 +0200

    diverse integrations

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+    // Add smooth scrolling
+    $("a.smooth").on('click', function(event) {
+
+      if (this.hash !== "") {
+        event.preventDefault();
+  
+  
+        var hash = this.hash;
+  
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+  
+          window.location.hash = hash;
+        });
+      }
+    });
+  });
